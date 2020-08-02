47053ccb0e53d267067bb1e971bd1338364985ce
asgerf <asgerf@github.com>
2020-08-03 00:08:09 +0100

    Add option to paste file from clipboard

--- src/web/flamegraph_webmain.ts
+++ src/web/flamegraph_webmain.ts
@@ -1,15 +1,14 @@
 /// <reference lib="DOM"/>
 
 import { select } from 'd3-selection';
-import type { FlamegraphNode } from '../common/flamegraph_builder';
+import { FlamegraphNode, getFlamegraphFromLogText } from '../common/flamegraph_builder';
 import escape = require('lodash.escape');
 import d3f = require('d3-flame-graph');
 
-// Declare values brought in through script tags.
-declare var codeqlFlamegraphData: FlamegraphNode;
-
 const detailsView = document.getElementById('details')!;
 const tupleCountView = document.getElementById('tuple-count-view')!;
+const instructionsContainer = document.getElementById('instructions-container')!;
+const instructionsView = document.getElementById('instructions')!;
 
 var chart = d3f.flamegraph().width(960);
 
@@ -26,4 +25,25 @@ function showFlamegraph(rootNode: FlamegraphNode) {
         .datum(rootNode)
         .call(chart);
 }
-showFlamegraph(codeqlFlamegraphData);
+
+// If data was preloaded via a script tag, load that now.
+if ('codeqlFlamegraphData' in window) {
+    showFlamegraph((window as any).codeqlFlamegraphData as FlamegraphNode);
+} else {
+    instructionsContainer.style.display = 'block';
+
+    document.body.addEventListener('paste', event => {
+        event.preventDefault();
+        let paste: string = (event.clipboardData || (window as any).clipboardData).getData('text');
+        instructionsView.innerText = 'Loading...';
+        setTimeout(() => {
+            try {
+                showFlamegraph(getFlamegraphFromLogText(paste));
+                instructionsContainer.style.display = 'none';
+            } catch (e) {
+                instructionsContainer.innerText = 'Failed';
+                console.error(e);
+            }
+        }, 1);
+    });
+}
