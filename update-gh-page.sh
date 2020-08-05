ec69fa0c7bd218b17b320b5b2693e06af73f59b7
asgerf <asgerf@github.com>
2020-08-05 21:58:33 +0100

    Update update-gh-page.sh

---
+++ update-gh-page.sh
@@ -0,0 +1,24 @@
+set -e
+CurrentBranch=$(git rev-parse --abbrev-ref HEAD)
+
+if [[ $CurrentBranch != "main" ]]; then
+  echo "Not on main"
+  exit 1
+fi
+
+git checkout gh-pages
+git merge main
+
+yarn install
+rm -rf dist
+yarn build-example
+
+rm -rf docs
+mkdir docs
+cp dist/*.{js,css,html} ./docs/
+mv ./docs/flamegraph.html ./docs/index.html
+git add docs/*.{js,css,html}
+git commit -m 'Update gh-pages'
+git push
+
+git checkout $CurrentBranch
