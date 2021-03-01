73a38688c90c2f8e7c91a46adb80dc3667004000
Asger Feldthaus <asgerf@github.com>
2021-03-01 11:40:32 +0000

    Strip iteration count from predicate name

--- src/common/flamegraph_builder.ts
+++ src/common/flamegraph_builder.ts
@@ -37,7 +37,7 @@ class PredicateNode {
 }
 
 function rewritePredicateName(name: string) {
-    return name.replace(/(#(cur_delta|prev_delta|prev)|@staged_ext|_delta)$/, '');
+    return name.replace(/(#(cur_delta|prev_delta|prev)|@staged_ext|_delta)$|@i\d+\b/, '');
 }
 
 export class FlamegraphBuilder {
